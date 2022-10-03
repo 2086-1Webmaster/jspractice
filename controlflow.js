6080d387a4414f2dd6723bd8e961928e573bd111
ManishA4 <79549344+2086-1Webmaster@users.noreply.github.com>
2022-10-03 13:27:57 +0000

    grahhh

--- controlflow.js
+++ controlflow.js
@@ -1,5 +1,21 @@
-for(let i = 0, i < 5; i++){
-  console.log('in loop', i);
-}
+//for(let i = 0; i < 5; i++){
+  //console.log('in loop:', i);
+//}
 
-  console.log('loop finished');
+//console.log('loop finished');
+
+//const names = ['shawn', 'mario', 'luigi'];
+
+//for(let i = 0; i < names.length; i++){
+
+  //console.log(names[i]);
+
+ // let html =  `<div>${names[i]}</div>`;
+ // console.log(html);
+//}
+
+let i = 0;
+for(let i < 5){
+
+  console.log('in oop: ' i);
+}
