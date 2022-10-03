8152baf520ade913c2719214722b472678285618
ManishA4 <79549344+2086-1Webmaster@users.noreply.github.com>
2022-10-03 13:02:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,140 @@
+//let email = 'mario@thenetninja.co.uk';
+
+
+//let result = email.substr(4, 10);
+
+//let result = email.replace('m' , 'w');
+
+//let result = email.replace('n', 'w')
+
+//console.log(result);
+
+
+//let radius = 10;
+//const pi = 3.14;
+
+//console.log(radius, pi);
+
+//let result = pi * radius**2;
+
+//let result = 5 * (10-3)**2;
+
+//console.log(result);
+
+//let likes = 10; 
+
+//likes **= 2;
+
+//console.log(likes);
+
+
+//console.log(5 / "hello");
+
+//let result = 'the blog has ' + likes + ' likes';
+
+//console.log(result);
+
+//const title = 'Best reads of 2019';
+//const author = 'Mario';
+//const likes = 30;
+
+
+//let result = 'The blog called '+ title + ' by ' + author + ' has ' + likes + ' likes '; 
+
+//console.log(result);
+
+//let result = `The blog is called ${title} by ${author} has ${likes} likes`;
+//console.log(result);
+
+//let html = `
+//<h2>${title}</h2>
+//<p>By ${author}</p>
+//span>This blog has ${likes} likes</span>
+
+//`;
+
+//console.log(html);
+
+//let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+//ninjas[1] = 'ken';
+//console.log(ninjas[1]);
+
+//let ages = [20,25,30,35];
+//console.log(ages[2]);
+
+//let random = ['shaun', 'crystal', 30, 20];
+//console.log(random);
+
+//console.log(ninjas.length);
+
+//array methods
+
+//let result = ninjas.join(', ')
+
+//let result = ninjas.indexOf('chun-li');
+
+//let result = ninjas.concat(['ken', 'crystal'])
+
+//let result = ninjas.push('ken');
+//result = ninjas.pop();
+
+//console.log(result);
+
+//let age = null; 
+
+//onsole.log(age, age + 3, `the age is ${age}`);
+
+//console.log(true, false, "true", "false");
+
+//let email = "luigi@netninja.com";
+//let names = ['mario', 'luigi', 'toad'];
+
+
+//let result = email.includes('!');
+//let result = names.includes('bowser');
+
+//console.log(result);
+
+//let age = 25; 
+
+//console.log(age == 25);
+//console.log(age == 14);
+//console.log(age != 14);
+//console.log(age > 20);
+//console.log(age < 20);
+//console.log(age <= 25);
+//console.log(age >= 20);
+
+//let name = 'shaun';
+//console.log(name == 'shaun');
+//console.log(name == 'Shaun');
+//console.log(name > 'crystal');
+//console.log(name > 'Shaun');
+
+
+//let age = 25; 
+
+//console.log(age == 25);
+//console.log(age == '25');
+//console.log(age != 25);
+//console.log(age != '25');
+
+//console.log(age === 25);
+//console.log(age === '25');
+//console.log(age !== 25);
+//console.log(age !== '25');
+
+//let score = '100';
+//score = Number(score);
+
+//console.log(score + 1);
+//console.log(typeof score);
+
+//let result = Number('hello');
+
+//let result = String(50);
+
+//let result = Boolean('');
+//console.log(result, typeof result);
+
