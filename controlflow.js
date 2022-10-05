ba28df52b1c631b63e441bc6110a81aa226be319
ManishA4 <79549344+2086-1Webmaster@users.noreply.github.com>
2022-10-05 22:23:39 +0000

    finished functions video

--- controlflow.js
+++ controlflow.js
@@ -14,8 +14,117 @@
  // console.log(html);
 //}
 
-let i = 0;
-for(let i < 5){
+//const names = ['shaun', 'mario', 'luigi', 'guing', 'jonathan'];
 
-  console.log('in oop: ' i);
-}
+
+//let i = 0;
+//while(i <= 5){
+  //console.log('in loop: ', i);
+  //i++
+//}
+
+//let i = 0;
+//while(i < names.length){
+ // console.log(names[i]);
+ // i++;
+//}
+
+//let i = 4;
+
+//do{
+//  console.log('val of i is: ', i);
+//  i++;
+//} while(i < 5);
+
+
+//const age = 20;
+
+//if(age > 20){
+//  console.log('you are over 20 years old');
+  
+//}
+
+//const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+//if(ninjas.length > 3){
+//  console.log("that's a lot of ninjas");
+//}
+
+//const password = 'password';
+
+//if(password.length >= 12 && password.includes("@")){
+ // console.log('that password is very good');
+//} else if(password.length >= 8 || password.includes('@')){
+ // console.log('that password is good');
+//} else {
+ // console.log('password is not good');
+//}
+
+//let user = false;
+
+//if(!user){
+ // console.log('you must be logged in to continue');
+//}
+
+
+
+//console.log(!true);
+//console.log(!false);
+
+//const scores = [50, 25, 0, 30, 100, 20, 10];
+
+//for(let i=0; i < scores.length; i++){
+
+  //if(scores[i] === 0){
+    //continue;
+  //}
+  
+  //console.log('your score: ', scores[i]);
+
+  //if(scores[i] === 100){
+  //  console.log('congrats, you got the top score!');
+  //  break;
+  //}
+//}
+
+//const grade = "C";
+
+//switch(grade){
+  //case "A":
+    //console.log('you got an A');
+    //break;
+ // case "B":
+   // console.log('you got an B');
+  //  break;
+ // case "C":
+ //   console.log('you got an C');
+//    break;
+ // case "D":
+//    console.log('you got an D');
+//    break;
+//  case "E":
+//    console.log('you got an E');
+ //   break;
+//  default:
+//    console.log('not a valid grade');
+//}
+
+//const age = 30;
+
+//let age = 50;
+
+
+
+//if(true){
+ // const age = 40;
+//  const name = 'shaun';
+//  console.log('outside code block: ', age, name);
+
+//  if(true){
+//    const age = 50;
+ //   console.log('inside 2nd code block: ', age);
+ // }
+//}
+
+//console.log('outside code block: ', age, name);
+
---
+++ functions.js
@@ -0,0 +1,104 @@
+//function greet(){
+  //console.log('hello there');
+//}
+
+
+//const speak = function(){
+  //console.log('good day');
+//};
+//greet();
+//greet();
+//greet();
+
+//speak();
+//speak();
+//speak();
+
+
+//const speak = function(name = 'luigi', time = 'night'){
+ // console.log(`good day ${time} ${name}`);
+//};
+
+//const calcArea = function(radius){
+//return 3.14 * radius**2;
+//};
+
+//const calcArea = (radius) => 3.14 * radius**2;
+
+//const area = calcArea(5);
+//console.log('area is: ', area);
+
+
+//const greet = function(){
+  //return 'hello, world';
+//}
+
+//const greet = () => 'hello, world';
+//const result = greet();
+
+//console.log(result);
+
+//const bill = function(products, tax){
+ // let total = 0;
+ // for(let i = 0; i < products.length; i++){
+ //   total += products[i] + products[i] * tax;
+ // }
+ // return total;
+//}
+
+//const bill = (products, tax) => {
+
+  //let total = 0; 
+ // for(let i = 0; i < products.length; i++){
+ //   total += products[i] + products[i] * tax;
+ // }
+ // return total;
+//};
+
+
+//console.log(bill([10, 15, 30], 0.006));
+
+
+//const name = 'shaun';
+
+//const greet = () => 'hello';
+
+
+//let resultOne = greet();
+//console.log(resultOne);
+
+
+//let resultTwo = name.toUpperCase();
+//console.log(resultTwo);
+
+
+//const myFunc = (callbackFunc) => {
+ // let value = 50;
+ // callbackFunc(value);
+//};
+
+//myFunc( value => {
+//  console.log(value);
+//});
+
+//let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+//const logPerson = (person, index) =>{
+ //console.log(`${index} - hello ${person}`);
+//};
+
+//people.forEach(logPerson);
+
+//const ul = document.querySelector('.people');
+
+//const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+//let html = ``;
+
+//people.forEach(function(person => {
+//  html += `<li style="color: purple">${person}</li>`;
+//});
+
+//console.log(html);
+
+//ul.innerHTML = html;
