63f8493643dfcce9d190c370cba1564528a66270
ManishA4 <79549344+2086-1Webmaster@users.noreply.github.com>
2022-10-06 15:26:27 +0000

    finished objects

---
+++ objects.js
@@ -0,0 +1,91 @@
+//const blogs = [
+ // { title: 'I do not like bread', likes: 30},
+ // { title: '10 reasons I do not like bread', likes: 50}
+//];
+
+//console.log(blogs);
+
+
+//let user = {
+ // name: 'crystal',
+//  age: 30,
+//  email: 'gmail@gmail.com',
+//  location: 'berlin',
+//  blogs: [
+ //   { title: 'I do not like bread', likes: 30},
+//  { title: '10 reasons I do not like bread', likes: 50}
+  
+//   ],
+
+//   login(){
+//       console.log('the user logged       in');
+//   },
+//   logout(){
+//     console.log('the user logged       out');
+//   },
+//   logBlogs(){
+//     //console.log(this.blogs)
+//     console.log('this user has         written the following blogs:       ');
+//     this.blogs.forEach(blog => {
+//       console.log(blog.title, blog.likes);
+//     })
+//   }
+// };
+
+// user.logBlogs();
+// console.log(this);
+
+//console.log(user);
+//console.log(user.name);
+
+//user.age = 35; 
+//console.log(user.age);
+
+//console.log(user['location']);
+//user['name'] = 'chun-li';
+//console.log(user['name']);
+
+//console.log(typeof user);
+
+//user.login();
+//user.logout();
+
+//const name = 'mario';
+//name.toUpperCase();
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// const area = 7.7;
+
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
+
+
+// const random = Math.random();
+
+// console.log(random);
+// console.log(Math.round(random * 100));
+
+
+// let scoreOne = 50; 
+// let scoreTwo = scoreOne;
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// scoreOne = 100; 
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+
+// userOne = { name: 'ryu', age: 30 };
+// userTwo = userOne;
+
+// console.log(userOne, userTwo);
+
+// userOne.age = 40; 
+
+// console.log(userOne, userTwo);
