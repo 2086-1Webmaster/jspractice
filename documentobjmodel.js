5a8380ee6c5a8e6a132ab9b4c8e86c764431f921
ManishA4 <79549344+2086-1Webmaster@users.noreply.github.com>
2022-10-06 15:53:17 +0000

    started video 6

---
+++ documentobjmodel.js
@@ -0,0 +1,20 @@
+// const para = document.querySelector('.error');
+
+// // console.log(para);
+
+// const paras = document.querySelectorAll('p');
+
+// // paras.forEach(para => {
+// //   console.log(para);
+// // });
+
+// const errors = document.querySelectorAll('.error')
+
+// console.log(errors);
+
+
+const title = document.getElementById('page-title');
+
+console.log(title);
+
+const errors = document.getElementsByClassName('.error');
