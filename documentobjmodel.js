de47c95ce1663baff87e1a845930ef290603b90d
ManishA4 <79549344+2086-1Webmaster@users.noreply.github.com>
2022-10-10 13:18:55 +0000

    I finsihed

--- documentobjmodel.js
+++ documentobjmodel.js
@@ -13,8 +13,58 @@
 // console.log(errors);
 
 
-const title = document.getElementById('page-title');
+// const title = document.getElementById('page-title');
 
-console.log(title);
+// console.log(title);
 
-const errors = document.getElementsByClassName('.error');
+// const errors =document.getElementsByClassName('.error');
+
+
+// console.log(errors);
+// console.log(errors[0]);
+
+// errors.forEach
+
+// const paras = document.getElementsByTagName('p');
+
+// console.log(paras);
+
+// console.log(paras[0]);
+
+// const para = document.querySelector("p");
+
+//console.log(para.innerText);
+
+//para.innerText = 'ninjas are awesome!';
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para => {
+//   console.log(para.innerText);
+//   para.innerText += ' new text';
+// })
+
+// const content = document.querySelector(".content");
+
+// //console.log(content.innerHTML);
+
+// // content.innerHTML += "<h2>This is a new h2</h2>";
+
+// const people = ['mario', 'luigi', 'yoshi'];
+
+// people.forEach(person =>{
+//   content.innerHTML += `<p>${person}</p>`;
+// })
+
+// const link = document.querySelector('a');
+
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk');
+
+// link.innerText = 'TheNet Ninja Website ';
+
+// const mmsg = document.querySelector('p');
+
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class', 'success');
+
+
